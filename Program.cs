71598c0d1680110bf5dbc3d1f1fd811f445fa1cc
Kağan Öktem <kagan.oktem@hotmail.com>
2021-03-15 03:38:06 +0300

    fixx

--- Program.cs
+++ Program.cs
@@ -38,8 +38,7 @@ namespace ReadingIsGood
                     {
                         logging.ClearProviders();
                         logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                    }).UseNLog().UseKestrel().UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT")).
-                        Build();
+                    }).UseNLog();  // NLog: setup NLog for Dependency injection;
                 });
     }
 }
